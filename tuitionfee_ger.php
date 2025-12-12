9a7928d7f041b00c8a53799cc1cf5c5ca5097b78
Nazifa-758 <rawshanniva14@gmail.com>
2025-12-12 17:01:49 +0600

    Create Tuition Fee button for Germany

---
+++ tuitionfee_ger.php
@@ -0,0 +1,35 @@
+<?php
+// Include the database connection file
+include('connect.php');
+
+// Define the query to fetch university details based on tuition fee categories for German universities
+$query = "SELECT 
+        u.name AS University_Name,
+        u.global_rank AS World_Rank,
+        GROUP_CONCAT(DISTINCT REPLACE(p.name, ' Programs', '') SEPARATOR ', ') AS Program_Type,
+        tf.fee_category AS Tuition_Fee_Type,
+        u.location AS Location
+    FROM 
+        University u
+    INNER JOIN 
+        UniversityProgramField upf ON u.university_id = upf.university_id
+    INNER JOIN 
+        Program p ON upf.program_id = p.program_id
+    INNER JOIN 
+        tuitionfee tf ON u.university_id = tf.university_id
+    WHERE 
+        u.country_id = (
+            SELECT country_id 
+            FROM CountryList 
+            WHERE name = 'Germany'
+        )
+        AND u.name != 'charité universitätsmedizin berlin'
+    GROUP BY 
+        u.university_id, tf.fee_category
+    ORDER BY 
+        FIELD(tf.fee_category, 'Free', 'Medium', 'High')
+";
+
+// Execute the query
+$result = $conn->query($query);
+?>
